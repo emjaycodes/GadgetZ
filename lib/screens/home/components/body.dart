f47f658baff10eb69cc7f469e20beedf5cc1f66a
JUSTICE <justicenwogu8@gmail.com>
2022-08-25 14:20:47 +0100

    created search bar in homeScreen

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,17 +1,31 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/material.dart'; 
-import 'package:oga_bassey/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:oga_bassey/constants.dart'; 
+
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-      
-     ),
+    return Column(
+      children: [
+        kbigSizedbox,
+        TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              hintStyle: TextStyle(
+                color: kprimaryColor,
+              ),
+              
+              border: OutlineInputBorder(
+                // borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(100)
+              )
+            ),
+        ),
+      ],
     );
   }
 }
