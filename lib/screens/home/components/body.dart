19f3cbaf009702238ce3d5d4e619ff35290d19b3
JUSTICE <justicenwogu8@gmail.com>
2022-08-25 16:41:38 +0100

    created a information box

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:oga_bassey/constants.dart'; 
+import 'package:oga_bassey/constants.dart';
+import '../../../size_cofig.dart'; 
 
 
 class Body extends StatelessWidget {
@@ -9,22 +10,62 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Column(
       children: [
         kbigSizedbox,
-        TextField(
-            decoration: InputDecoration(
-              hintText: 'Search',
-              hintStyle: TextStyle(
-                color: kprimaryColor,
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  color: Color(0xFF7F90A0),
+                  onPressed: (){}, icon:
+                 Icon(Icons.search)
+                 ),
+                hintText: 'Search',
+                hintStyle: TextStyle(
+                  color: ktertiaryColor,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20)
+                )
               ),
-              
-              border: OutlineInputBorder(
-                // borderSide: BorderSide.none,
-                borderRadius: BorderRadius.circular(100)
-              )
-            ),
+          ),
         ),
+
+        ksmallSizedbox,
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xFFFDE6E7)
+              ),
+              height: 52,
+              width: 356,
+              child: Padding(
+                padding: const EdgeInsets.only(left:8.0),
+                child: Row(
+                  children: [ 
+                     Icon(Icons.assignment_late_outlined
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Complete your profile, add delivery details'),
+
+                     IconButton(onPressed: (){},
+                     icon: Icon(Icons.assignment_late_outlined
+                    ) ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
