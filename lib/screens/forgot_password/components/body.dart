c3f711229f7e737319df90b274340ddc3cdc32ce
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:10:28 +0100

    refactored the code

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -11,95 +11,94 @@ import 'package:oga_bassey/screens/login/login.dart';
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children:  [
-             //text
-             kbigSizedbox,
-             Row(
+          children: [
+            //text
+            kbigSizedbox,
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
-               children: [
-                 Padding(
-                   padding: const EdgeInsets.symmetric(horizontal: 33),
-                   child: Text('Forgot password',
-                   style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: kprimaryColor,
-                    )
-                   ),
-                 ),
-               ],
-             ),
-             
-             kbigSizedbox,
-          //text
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 35.0),
-            child: Text('Enter the email address associated wth your account to reset your password.',
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 13,
-              color: ksecondaryTextColor
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 33),
+                  child: Text('Forgot password',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: kprimaryColor,
+                      )),
+                ),
+              ],
             ),
-            ),
-          ),
 
-          kbigSizedbox,
-
-          //textfield
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextfieldName(text: 'Email address'),
-          ),
-
-          ksmallSizedbox,
-          EmailTextfield(),
-          kbigSizedbox,
-          CustomButtom(
-            buttonName: "Forgot password", 
-            buttonColor: kprimaryColor, 
-            textStyle: kloginbuttonTextStyle(), 
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> EmailNotifierScreen()));
-            }),
-
-          kbigSizedbox,
-          //text
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Return to',
-              style: TextStyle(
-                color: ksecondaryTextColor,
-                fontWeight: FontWeight.w500,
-                fontSize: 12,
-
-              ),
-              ),
-
-              GestureDetector(
-                onTap: (){
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
-                child: Text(' Login',
+            kbigSizedbox,
+            //text
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+              child: Text(
+                'Enter the email address associated wth your account to reset your password.',
                 style: TextStyle(
-                  color: kprimaryColor,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 13,
+                    color: ksecondaryTextColor),
+              ),
+            ),
+
+            kbigSizedbox,
+
+            //textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextfieldName(text: 'Email address'),
+            ),
+
+            ksmallSizedbox,
+            EmailTextfield(),
+            kbigSizedbox,
+            CustomButtom(
+                buttonName: "Forgot password",
+                buttonColor: kprimaryColor,
+                textStyle: kloginbuttonTextStyle(),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => EmailNotifierScreen()));
+                }),
+
+            kbigSizedbox,
+            //text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Return to',
+                  style: TextStyle(
+                    color: ksecondaryTextColor,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ),
                 ),
-                ),
-              )
-            ],
-          )
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  child: Text(
+                    ' Login',
+                    style: TextStyle(
+                      color: kprimaryColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 12,
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
-         
         ),
-        
       ),
     );
   }
