96ba2f98e5b0794dde9b6169640bf069e4b6fd71
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:06:39 +0100

    refactored code

--- lib/components/textfield_names.dart
+++ lib/components/textfield_names.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:oga_bassey/constants.dart';
 
-
 class TextfieldName extends StatelessWidget {
-
   // ignore: use_key_in_widget_constructors
-  const TextfieldName({ required this.text, });
+  const TextfieldName({
+    required this.text,
+  });
 
   final String text;
 
-  
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 35),
       child: Row(
         children: [
-        Text( text,
-      style: textboxNameStyle(),
-      ),
+          Text(
+            text,
+            style: textboxNameStyle(),
+          ),
         ],
       ),
     );
