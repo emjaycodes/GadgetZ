8e14d93f0e4bc346c065d54b87593c38f54c10bc
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:06:15 +0100

    refactored code

--- lib/components/social_media_card.dart
+++ lib/components/social_media_card.dart
@@ -6,26 +6,23 @@ class SocialMediaCard extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  final ImageProvider socialImage; 
+  final ImageProvider socialImage;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){print('i got pressed');},
+      onTap: () {
+        print('i got pressed');
+      },
       child: Card(
         elevation: 5,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15)
-          ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         child: Container(
           width: 70,
           height: 70,
           decoration: BoxDecoration(
             boxShadow: const [
-              BoxShadow(
-                blurRadius: 25.0,
-                color: Color.fromRGBO(0, 0, 0, 0.1)
-              ),
+              BoxShadow(blurRadius: 25.0, color: Color.fromRGBO(0, 0, 0, 0.1)),
             ],
             image: DecorationImage(
               image: socialImage,
