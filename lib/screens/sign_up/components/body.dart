1faa6d259769680f1901cebeb8d8d4c56ec385ae
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:13:20 +0100

    refactored the code

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
@@ -10,123 +9,117 @@ import 'package:oga_bassey/components/social_media_card.dart';
 import 'package:oga_bassey/screens/forgot_password/forgot_password.dart';
 import 'package:oga_bassey/screens/home/home_screen.dart';
 
-
-
-class Body  extends StatelessWidget {
-   const Body ({Key? key}) : super(key: key);
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child:SingleChildScrollView(
-        child: Column(
-          children: [
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
           kdivider,
-            kbigSizedbox,
-            // text that says create account
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 100),
-              child: Text('Create your account',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500
-              )
-              ),
-            ),
+          kbigSizedbox,
+          // text that says create account
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: Text('Create your account',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+          ),
 
-            kbigSizedbox,
-            TextfieldName(text: 'Full name',),
-            
-                 //textfield 
-                 
-                FullnameTextfield(),
-      
-                 SizedBox(height: 20,),
-                 // that says email 
-                 TextfieldName(text: 'Email address',),
-                 SizedBox(height: 10,),
-                 // textfield
-                 EmailTextfield(),
-                  kbigSizedbox,
-                 // text that says passsword
-                 TextfieldName(text: 'Password',),
-                 ksmallSizedbox,
-                 // textfield with icon
-                 PasswordTextfield(),
+          kbigSizedbox,
+          TextfieldName(
+            text: 'Full name',
+          ),
 
-                 kbigSizedbox,
-                 
-                // sign up buttun
-                CustomButtom(
-                 buttonName: 'Signup',
-                 buttonColor: kdisabledButtonColor, 
-                 textStyle: ksignupbuttonTextStyle(),
-                 onPressed: (){
-                  Navigator.pushNamed(context, HomeScreen.id);
-                 }),
-      
-              kbigSizedbox,
-      
-              
-              // row wit divder with a text 'row'
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                height: 7,
-                width: 169,
-                child: Divider(
-                  thickness: 4,
-                  color: Color(0xFFF5F5F5),
-                  // indent: 1,
-                  endIndent: 10,
-                )
-              ),
-                  
-                  // SizedBox(width: 45),
-                  // text 'or'
-                  Text('OR'),
-      
-                  SizedBox(
-                height: 7,
-                width: 175,
-                child: Divider(
-                  thickness: 5,
-                  color: Color(0xFFF5F5F5),
-                  indent: 10,
-                )
-              ),
-                ],
-              ),
+          //textfield
 
-              kbigSizedbox,
-              //row with three svg buttons
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SocialMediaCard(socialImage: AssetImage('images/google.png'),),
+          FullnameTextfield(),
 
-                    SocialMediaCard(socialImage: AssetImage('images/apple.png'),),
+          SizedBox(
+            height: 20,
+          ),
+          // that says email
+          TextfieldName(
+            text: 'Email address',
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // textfield
+          EmailTextfield(),
+          kbigSizedbox,
+          // text that says passsword
+          TextfieldName(
+            text: 'Password',
+          ),
+          ksmallSizedbox,
+          // textfield with icon
+          PasswordTextfield(),
 
-                    SocialMediaCard(socialImage: AssetImage('images/facebook.png'),)
-                  ],
+          kbigSizedbox,
+
+          // sign up buttun
+          CustomButtom(
+              buttonName: 'Signup',
+              buttonColor: kdisabledButtonColor,
+              textStyle: ksignupbuttonTextStyle(),
+              onPressed: () {
+                Navigator.pushNamed(context, HomeScreen.id);
+              }),
+
+          kbigSizedbox,
+
+          // row wit divder with a text 'row'
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 7,
+                  width: 169,
+                  child: Divider(
+                    thickness: 4,
+                    color: Color(0xFFF5F5F5),
+                    // indent: 1,
+                    endIndent: 10,
+                  )),
+
+              // SizedBox(width: 45),
+              // text 'or'
+              Text('OR'),
+
+              SizedBox(
+                  height: 7,
+                  width: 175,
+                  child: Divider(
+                    thickness: 5,
+                    color: Color(0xFFF5F5F5),
+                    indent: 10,
+                  )),
+            ],
+          ),
+
+          kbigSizedbox,
+          //row with three svg buttons
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SocialMediaCard(
+                  socialImage: AssetImage('images/google.png'),
                 ),
-              ),
-                  ],
-        ),
-      )
-    );
-      
-
-
-      
-      
-
- 
-    
+                SocialMediaCard(
+                  socialImage: AssetImage('images/apple.png'),
+                ),
+                SocialMediaCard(
+                  socialImage: AssetImage('images/facebook.png'),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
   }
-
 }
-
