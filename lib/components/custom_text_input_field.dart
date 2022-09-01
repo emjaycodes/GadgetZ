533228ff0580a176bc875680ae69518f6aae47bc
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:05:18 +0100

    refactored code

--- lib/components/custom_text_input_field.dart
+++ lib/components/custom_text_input_field.dart
@@ -16,32 +16,33 @@ class _PasswordTextfieldState extends State<PasswordTextfield> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-     width: 339,
+      width: 339,
       child: Form(
         autovalidateMode: AutovalidateMode.onUserInteraction,
         child: TextFormField(
           obscureText: hidePassword,
-         decoration: InputDecoration(
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.remove_red_eye_outlined),
-            onPressed: (){
-              setState(() {
-                if (hidePassword = true) {
-                  hidePassword = false;
-                }
-              });
-            },
-          ),
-           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))
-           // border: InputBorder.none
-         ),
-         validator: (value){
-          if(value != null && value.length < 7){
-            return 'Enter min. 6 Characters'; //
-          } else {
-            return null;
-          }
-         },
+          decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.remove_red_eye_outlined),
+                onPressed: () {
+                  setState(() {
+                    if (hidePassword = true) {
+                      hidePassword = false;
+                    }
+                  });
+                },
+              ),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))
+              // border: InputBorder.none
+              ),
+          validator: (value) {
+            if (value != null && value.length < 7) {
+              return 'Enter min. 6 Characters'; //
+            } else {
+              return null;
+            }
+          },
         ),
       ),
     );
@@ -56,24 +57,23 @@ class EmailTextfield extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-     width: 339,
+      width: 339,
       child: Form(
         autovalidateMode: AutovalidateMode.onUserInteraction,
         child: TextFormField(
-         decoration: InputDecoration(
-           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))
-           // border: InputBorder.none
-         ),
-         validator: (email)=>  
-         email != null && !EmailValidator.validate(email)
-         ? 'Enter a valid email'
-         : null
-        ),
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0))
+                // border: InputBorder.none
+                ),
+            validator: (email) =>
+                email != null && !EmailValidator.validate(email)
+                    ? 'Enter a valid email'
+                    : null),
       ),
     );
   }
 }
-
 
 class FullnameTextfield extends StatelessWidget {
   const FullnameTextfield({
@@ -83,14 +83,15 @@ class FullnameTextfield extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-     width: 339,
+      width: 339,
       child: Form(
         autovalidateMode: AutovalidateMode.onUserInteraction,
         child: TextFormField(
-         decoration: InputDecoration(
-           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))
-           // border: InputBorder.none
-         ),
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))
+              // border: InputBorder.none
+              ),
         ),
       ),
     );
