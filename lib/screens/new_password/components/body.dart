524c5698096b1759027cde184655ac33c68b3d40
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:12:54 +0100

    refactored the code

--- lib/screens/new_password/components/body.dart
+++ lib/screens/new_password/components/body.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
@@ -7,61 +6,65 @@ import 'package:oga_bassey/components/custom_text_input_field.dart';
 import 'package:oga_bassey/components/textfield_names.dart';
 import 'package:oga_bassey/components/custom_button.dart';
 
-
-
-class Body  extends StatelessWidget {
-   const Body ({Key? key}) : super(key: key);
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child:SingleChildScrollView(
-        child: Column(
-          children: [
-            kbigSizedbox,
-            // text that says create new pasword
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 100),
-              child: Text('Create new password',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500
-              )
-              ),
-            ),
-            
-            kbigSizedbox,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Text('Your new password must be differnt from previous used passwords',
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          kbigSizedbox,
+          // text that says create new pasword
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: Text('Create new password',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+          ),
+
+          kbigSizedbox,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              'Your new password must be differnt from previous used passwords',
               style: textboxNameStyle(),
-              ),
             ),
+          ),
 
-            kbigSizedbox,
-           
-                 SizedBox(height: 20,),
-                 // that says new password 
-                 TextfieldName(text: 'New password',),
-                 SizedBox(height: 10,),
-                 // textfield
-                 PasswordTextfield(),
-                  kbigSizedbox,
-                 // text that says passsword
-                 TextfieldName(text: 'Confirm password',),
-                 ksmallSizedbox,
-                 // textfield with icon
-                 PasswordTextfield(),
-                 
-                 kbigSizedbox,
-      
-                // sign up buttun
-                CustomButtom(buttonName: 'Reset password', buttonColor: kprimaryColor, textStyle: kloginbuttonTextStyle(), onPressed: (){})
-                  ],
-        ),
-      )
-    );
+          kbigSizedbox,
+
+          SizedBox(
+            height: 20,
+          ),
+          // that says new password
+          TextfieldName(
+            text: 'New password',
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // textfield
+          PasswordTextfield(),
+          kbigSizedbox,
+          // text that says passsword
+          TextfieldName(
+            text: 'Confirm password',
+          ),
+          ksmallSizedbox,
+          // textfield with icon
+          PasswordTextfield(),
+
+          kbigSizedbox,
+
+          // sign up buttun
+          CustomButtom(
+              buttonName: 'Reset password',
+              buttonColor: kprimaryColor,
+              textStyle: kloginbuttonTextStyle(),
+              onPressed: () {})
+        ],
+      ),
+    ));
   }
-
 }
-
