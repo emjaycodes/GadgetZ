84ca38dfb81f5d6edb0b65c636ae287a91ee9c1e
JUSTICE <justicenwogu8@gmail.com>
2022-09-01 16:12:22 +0100

    refactored the code

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -3,7 +3,6 @@ import 'package:oga_bassey/constants.dart';
 import 'package:oga_bassey/screens/welcome.dart';
 import 'components/body.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -19,22 +18,22 @@ class _LoginPageState extends State<LoginScreen> {
     // ignore: prefer_const_constructors
     return Scaffold(
       backgroundColor: Colors.white,
-    // appbar with a button arrow back button
+      // appbar with a button arrow back button
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(
-                context,
-              );
-            },
-            icon: const Icon(
-              Icons.navigate_before,
-              color: ksecondaryTextColor,
-              size: 40,
-            )
-            ),
+          onPressed: () {
+            Navigator.pop(
+              context,
+            );
+          },
+          icon: const Icon(
+            Icons.navigate_before,
+            color: ksecondaryTextColor,
+            size: 40,
+          ),
+        ),
       ),
 
       body: const Body(),
