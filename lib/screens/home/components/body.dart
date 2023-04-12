80b5836ab128d6f4d98d046e1be27b6b86eca4d1
emjaycodes <justicenwogu8@gmail.com>
2023-04-12 00:00:31 -0700

    created scrollable buttons

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -27,18 +27,40 @@ class Body extends StatelessWidget {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
-                    Stack(
-                      children :[
-                        Container(
-                        height: 60,
-                        width: 130,
-                        decoration: BoxDecoration(
-                            color: kprimaryColor,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Text('Laptop'),
-                      Text('40 Products')
-                      ]
+                    SizedBox(
+                      width: 15,
+                    ),
+                    horizontalStackScroll(
+                      gadgetType: 'Laptop',
+                      productAmount: '40 Products', productImage: 'images/zen.png',
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    horizontalStackScroll(
+                      gadgetType: 'Laptop',
+                      productAmount: '40 Products', productImage: 'images/zen.png',
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    horizontalStackScroll(
+                      gadgetType: 'Laptop',
+                      productAmount: '40 Products', productImage: 'images/zen.png',
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    horizontalStackScroll(
+                      gadgetType: 'Laptop',
+                      productAmount: '40 Products', productImage: 'images/zen.png',
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    horizontalStackScroll(
+                      gadgetType: 'Laptop',
+                      productAmount: '40 Products', productImage: 'images/zen.png',
                     ),
                   ],
                 ),
@@ -48,5 +70,60 @@ class Body extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class horizontalStackScroll extends StatelessWidget {
+  final String gadgetType;
+  final String productAmount;
+  final String productImage;
+  const horizontalStackScroll({
+    super.key,
+    required this.gadgetType,
+    required this.productAmount, required this.productImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+      Container(
+        height: 60,
+        width: 140,
+        decoration: BoxDecoration(
+            color: kprimaryColor, borderRadius: BorderRadius.circular(15)),
+      ),
+      Positioned(
+        left: 60,
+        bottom: 43,
+        child: Text(
+          gadgetType,
+          style: TextStyle(
+            color: ktertiaryColor,
+          ),
+        ),
+      ),
+      Positioned(
+        left: 55,
+        bottom: 24,
+        child: Text(
+          productAmount,
+          style: TextStyle(
+            color: ktertiaryColor,
+          ),
+        ),
+      ),
+      Positioned(
+        left: 10,
+        bottom: 21,
+        child: Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+              color: ktertiaryColor, borderRadius: BorderRadius.circular(8)),
+          child: Center(child: Image.asset(productImage)),
+        ),
+      )
+    ]);
   }
 }
