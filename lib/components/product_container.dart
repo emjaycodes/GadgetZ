2c1334e898ed03112808a35b704c1a8a931a35d2
emjaycodes <justicenwogu8@gmail.com>
2023-04-13 04:32:04 -0700

    product container

---
+++ lib/components/product_container.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+
+class ProductContainer extends StatelessWidget {
+  final String productName;
+  final String productbrand;
+  final String productPrice;
+  final String productImage;
+
+  const ProductContainer({
+    super.key,
+    required this.productName,
+    required this.productbrand,
+    required this.productPrice,
+    required this.productImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 270,
+      width: 170,
+      decoration: BoxDecoration(
+          color: ktertiaryColor, borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        children: [
+          Image.asset(productImage),
+          Container(
+            height: 90,
+            width: 150,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ksmallSizedbox,
+                  Text(productName),
+                  ksmallSizedbox,
+                  Text(productbrand),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(productPrice),
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            color: ktertiaryColor,
+                            borderRadius: BorderRadius.circular(5)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
