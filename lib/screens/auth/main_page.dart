579d711e9057cfb8a0a35294f92d720351622048
emjaycodes <justicenwogu8@gmail.com>
2023-04-19 19:33:57 -0700

    created main page

---
+++ lib/screens/auth/main_page.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:oga_bassey/screens/auth/auth.dart';
+import 'package:oga_bassey/screens/home/home_screen.dart';
+import 'package:oga_bassey/screens/login_body.dart';
+
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+  static String id = 'main screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot){
+          if (snapshot.hasData){
+             return HomeScreen();
+          }
+          else {
+            return  LoginScreen();
+          }
+        },
+        
+        ),
+    );
+  }
+}
