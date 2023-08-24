83b640a3d31d16ac922bdbe82cbfef8a425aac8a
emjaycodes <justicenwogu8@gmail.com>
2023-08-24 14:35:20 +0100

    commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
 
 class ConstantKey {
   // ignore: constant_identifier_names
-  static const String PAYSTACK_KEY = "pk_test_291cd3614d62fa2ab67955bd533f1d210660212d";
+  static const String PAYSTACK_KEY =
+      "pk_test_291cd3614d62fa2ab67955bd533f1d210660212d";
 }
 
 const ksecondaryColor = Color(0xFFE30008);
@@ -17,36 +17,34 @@ const kwhiteColor = Colors.white;
 const kbigSizedbox = SizedBox(height: 20);
 const ksmallSizedbox = SizedBox(height: 10);
 
- TextStyle textboxNameStyle() {
-    return const TextStyle(
-            color: kprimaryColor,
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-          );
-  }
+TextStyle textboxNameStyle() {
+  return const TextStyle(
+    color: kprimaryColor,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+}
 
-TextStyle ksignupbuttonTextStyle(){
+TextStyle ksignupbuttonTextStyle() {
   return const TextStyle(
     fontSize: 15,
     color: Color(0xFF081658),
     fontWeight: FontWeight.w500,
-    );
+  );
 }
 
-TextStyle kloginbuttonTextStyle(){
+TextStyle kloginbuttonTextStyle() {
   return const TextStyle(
     fontSize: 15,
     color: ktertiaryColor,
     fontWeight: FontWeight.w500,
-    );
+  );
 }
-     
 
 const kdivider = SizedBox(
-              height: 12, 
-              width: double.infinity,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Color(0xFFF6F8FA)                  ),
-                  ),
-              );
+  height: 12,
+  width: double.infinity,
+  child: DecoratedBox(
+    decoration: BoxDecoration(color: Color(0xFFF6F8FA)),
+  ),
+);
